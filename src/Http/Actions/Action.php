f6ca2609faf6139257e11d5e89801c1ef6124970
Kjell <kjell@kejpa.com>
2026-06-12 21:02:08 +0300

    Lagt till tester för Action-basklasserna

--- src/Http/Actions/Action.php
+++ src/Http/Actions/Action.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Actions;
 
-use App\Domain\DomainException\DomainRecordNotFoundException;
+use App\Domain\Exception\DomainRecordNotFoundException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Log\LoggerInterface;
