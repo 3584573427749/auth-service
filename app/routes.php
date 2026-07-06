cbf7273512d6a2793d265b678573c74ee284a455
Kjell <kjell@kejpa.com>
2026-07-06 20:10:29 +0300

    Uppdaterat openapi och lagt till getRoleAction

--- app/routes.php
+++ app/routes.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Http\Actions\Roles\CreateRoleAction;
 use App\Http\Actions\Roles\GetAllRolesAction;
+use App\Http\Actions\Roles\GetRoleAction;
 use App\Http\Actions\User\CreateUserAction;
 use App\Http\Actions\User\DeleteUserAction;
 use App\Http\Actions\User\GetAllUsersAction;
@@ -11,7 +12,7 @@ use App\Http\Actions\User\GetUserAction;
 use App\Http\Actions\User\UpdateUserAction;
 use Slim\App;
 
-return function (App $app) : void {
+return function (App $app): void {
 
     $app->post('/users', CreateUserAction::class);
     $app->get('/users', GetAllUsersAction::class);
@@ -20,8 +21,8 @@ return function (App $app) : void {
     $app->delete('/users/{id}', DeleteUserAction::class);
     $app->post('/roles', CreateRoleAction::class);
     $app->get('/roles', GetAllRolesAction::class);
-    /*   $app->get('/roles/{id}', GetRoleAction::class);
-       $app->put('/roles/{id}', UpdateRoleAction::class);
+    $app->get('/roles/{id}', GetRoleAction::class);
+    /*   $app->put('/roles/{id}', UpdateRoleAction::class);
        $app->delete('/roles/{id}', DeleteRoleAction::class);
    */
 };
